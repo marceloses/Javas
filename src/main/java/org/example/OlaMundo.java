43820647cf7c2cbd37431dd43dc785d87f82721e
Marcelo Severo <git@github.com:marceloses/APICatalogo.git>
2023-09-21 22:35:35 -0300

    Versão 4

---
+++ src/main/java/org/example/OlaMundo.java
@@ -0,0 +1,13 @@
+package org.example;
+
+// Testando
+public class OlaMundo {
+    public static void main(String[] args) {
+
+        System.out.println("Vai Corinthians Campeão!!!");
+        System.out.println("Vamos São Paulo !!!");
+        System.out.println("Chora Porcada!!!");
+        System.out.println("Ninho do Urubbu Flamengo!!!");
+        System.out.println("Vasco Cruz de Malta!!!");
+    }
+}
